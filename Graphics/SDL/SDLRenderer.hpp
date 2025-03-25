4514fee8f100bc195511e52517da77d6cb5089d4
Yuuko <tilio.huart@epitech.eu>
2025-03-25 18:22:30 +0100

    feat: initialize SDLRenderer class

---
+++ Graphics/SDL/SDLRenderer.hpp
@@ -0,0 +1,15 @@
+//
+// EPITECH PROJECT, 2025
+// Arcade
+// File description:
+// SDLRenderer.hpp
+//
+
+#include "ARenderer.hpp"
+
+namespace ANAL {
+    class SDLRenderer : public ARenderer {
+       public:
+    };
+
+}  // namespace ANAL
