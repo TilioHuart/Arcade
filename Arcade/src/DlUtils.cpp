349b3af293b8e9798ded1d29fae0a7f6901303c6
Pomalone <o.c.loulou@gmail.com>
2025-03-31 14:54:31 +0200

    feat: add dlsym encapsulation

--- Arcade/src/DlUtils.cpp
+++ Arcade/src/DlUtils.cpp
@@ -6,6 +6,8 @@
 //
 
 #include "DlUtils.hpp"
+#include "IGame.hpp"
+#include "IModule.hpp"
 #include "dlfcn.h"
 #include <string>
 
@@ -22,4 +24,34 @@ void Arcade::DlUtils::close()
 {
     if (_lib == nullptr || dlclose(this->_lib) != 0)
         throw DlUtilsError("Invalid lib can't be dlclosed");
+}
+
+ANAL::ModuleType Arcade::DlUtils::getLibType()
+{
+    auto *const func = reinterpret_cast<ANAL::ModuleType(*)()>
+        (dlsym(this->_lib, "uwu_get_module_type"));
+    if (func == nullptr)
+        throw DlUtilsError("Couldn't retrieve lib type");
+    return func();
+}
+
+std::unique_ptr<ANAL::IGame> Arcade::DlUtils::loadGame()
+{
+    const auto func = reinterpret_cast<std::unique_ptr<ANAL::IGame>(*)()>
+        (dlsym(this->_game, "uwu_entrypoint_game"));
+    if (func == nullptr)
+        throw DlUtilsError("Couldn't retrieve game");
+    auto game = func();
+    return std::move(game);
+}
+
+std::unique_ptr<ANAL::IRenderer> Arcade::DlUtils::loadDisplay()
+{
+    const auto func = reinterpret_cast<std::unique_ptr<ANAL::IRenderer>(*)()>
+        (dlsym(this->_display, "uwu_entrypoint_renderer"));
+    if (func == nullptr)
+        throw DlUtilsError("Couldn't retrieve display");
+    auto renderer = func();
+    return std::move(renderer);
+
 }
