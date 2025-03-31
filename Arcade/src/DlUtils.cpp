e6ed44fb08e3e705d5f3c001ebc4dd754fad94b8
Pomalone <o.c.loulou@gmail.com>
2025-03-31 13:54:42 +0200

    feat: add dl functions

--- Arcade/src/DlUtils.cpp
+++ Arcade/src/DlUtils.cpp
@@ -5,18 +5,21 @@
 // DlFunctions.cpp
 //
 
-#include "DlFunctions.hpp"
-#include "IRenderer.hpp"
+#include "DlUtils.hpp"
 #include "dlfcn.h"
-#include "src/Arcade.hpp"
-#include <memory>
 #include <string>
 
 Arcade::DlUtils::DlUtilsError::DlUtilsError(std::string msg) : _msg(std::move(msg)) {}
 
-void Arcade::DlUtils::loadLib(const std::string &pathToLib)
+void Arcade::DlUtils::open(const std::string &pathToLib)
 {
     this->_lib = dlopen(pathToLib.c_str(), RTLD_LAZY);
     if (_lib == nullptr)
         throw DlUtilsError("Invalid pointer from dlopen");
+}
+
+void Arcade::DlUtils::close()
+{
+    if (_lib == nullptr || dlclose(this->_lib) != 0)
+        throw DlUtilsError("Invalid lib can't be dlclosed");
 }
