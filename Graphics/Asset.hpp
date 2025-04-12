35435817e16ff01fc1e21a5f4cfcc9473dc014f2
Yuuko <tilio.huart@epitech.eu>
2025-04-12 11:15:10 +0200

    doc: create asset class documentation

--- Graphics/Asset.hpp
+++ Graphics/Asset.hpp
@@ -12,45 +12,99 @@
 #include <memory>
 
 namespace ANAL {
-    class Asset : public IAsset {
-       public:
-        Asset() = default;
 
-        Asset(const Asset &asset) { *this = asset; }
+/**
+ * @class Asset
+ * @brief Represents a concrete implementation of IAsset.
+ *
+ * This class manages graphical asset resources by allowing the texture path to be set and retrieved,
+ * as well as an alternate render character.
+ */
+class Asset : public IAsset {
+   public:
+    /**
+     * @brief Default constructor.
+     */
+    Asset() = default;
 
-        virtual ~Asset() = default;
+    /**
+     * @brief Copy constructor.
+     * @param asset The Asset instance to copy from.
+     */
+    Asset(const Asset &asset) { *this = asset; }
 
-        Asset &operator=(const Asset &asset)
-        {
-            this->_texturePath = asset._texturePath;
-            this->_alt = asset._alt;
-            return *this;
-        }
+    /**
+     * @brief Virtual destructor.
+     */
+    virtual ~Asset() = default;
 
-        [[nodiscard]] std::unique_ptr<IAsset> clone() const
-        {
-            return std::make_unique<Asset>(*this);
-        }
+    /**
+     * @brief Assignment operator.
+     *
+     * Copies the texture path and the alternate render character from another asset.
+     *
+     * @param asset The asset to copy.
+     * @return A reference to the copied asset.
+     */
+    Asset &operator=(const Asset &asset)
+    {
+        this->_texturePath = asset._texturePath;
+        this->_alt = asset._alt;
+        return *this;
+    }
 
-        void setTexturePath(const std::string &texture) final
-        {
-            this->_texturePath = texture;
-        };
+    /**
+     * @brief Clones the current asset.
+     *
+     * Creates and returns a copy of the current asset.
+     *
+     * @return A unique pointer to the cloned asset.
+     */
+    [[nodiscard]] std::unique_ptr<IAsset> clone() const
+    {
+        return std::make_unique<Asset>(*this);
+    }
 
-        [[nodiscard]] const std::string &getTexturePath() const final
-        {
-            return this->_texturePath;
-        };
+    /**
+     * @brief Sets the texture path for the asset.
+     *
+     * @param texture A string representing the file path to the texture.
+     */
+    void setTexturePath(const std::string &texture) final
+    {
+        this->_texturePath = texture;
+    }
 
-        void setAlternateRender(char alt) final { this->_alt = alt; };
+    /**
+     * @brief Retrieves the texture path of the asset.
+     *
+     * @return A constant reference to the texture path string.
+     */
+    [[nodiscard]] const std::string &getTexturePath() const final
+    {
+        return this->_texturePath;
+    }
 
-        [[nodiscard]] char getAlternateRender() const final
-        {
-            return this->_alt;
-        };
+    /**
+     * @brief Sets the alternate render character for the asset.
+     *
+     * @param alt The character used as the alternate render.
+     */
+    void setAlternateRender(char alt) final { this->_alt = alt; }
 
-       private:
-        std::string _texturePath;
-        char _alt;
+    /**
+     * @brief Retrieves the alternate render character for the asset.
+     *
+     * @return The alternate render character.
+     */
+    [[nodiscard]] char getAlternateRender() const final
+    {
+        return this->_alt;
     };
+
+   private:
+    std::string _texturePath; ///< The texture path associated with the asset.
+    char _alt;                ///< The alternate render character.
+};
+
 }  // namespace ANAL
